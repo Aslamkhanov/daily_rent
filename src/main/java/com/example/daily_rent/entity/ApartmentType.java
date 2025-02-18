17985e3db01a9f02a78a5f49a40634cf8e5c0a52
Anbu <alihanovruslan83@gmail.com>
2025-02-18 18:30:36 +0300

    changed ApartmentType, added description

--- src/main/java/com/example/daily_rent/entity/ApartmentType.java
+++ src/main/java/com/example/daily_rent/entity/ApartmentType.java
@@ -1,5 +1,16 @@
 package com.example.daily_rent.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum ApartmentType {
-    ONLY_ROOM, ONE_ROOM, TWO_ROOM, THREE_ROOM, FOUR_OR_MORE_ROOM
+    ONLY_ROOM("комната"),
+    ONE_ROOM("1-комнатная"),
+    TWO_ROOM("2-комнатная"),
+    THREE_ROOM("3-комнатная"),
+    FOUR_OR_MORE_ROOM("4 и более комнатная");
+
+    private final String description;
 }
